76ba40b198be8e35016011458eb02434b9bd90ad
Priyanshu Sharma <priyanshush325@gmail.com>
2026-02-04 22:36:45 -0800

    feat: init

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func main () { 
+	router := gin.Default()
+	router.GET("/ping", func (c *gin.Context) {
+		fmt.Println("request received: %q", c.Request.Body)
+		c.JSON(200, gin.H {
+			"body": "pong",
+		})
+	})
+
+	router.Run()
+}
