bb83fc879707f17fd2b701b704efb7372d68b7a4
Priyanshu Sharma <priyanshush325@gmail.com>
2026-04-06 01:20:13 -0700

    feat: add function to pull user's most recently listened track from Spotify API

--- utils/request.go
+++ utils/request.go
@@ -5,6 +5,7 @@ import (
 		"encoding/json"
 		"fmt"
 		"net/http"
+		"time"
 )
 
 // Utility function to make Post Requests a lot easier. 
@@ -18,4 +19,28 @@ func RequestPost(requestUrl string, requestBody map[string]string) (*http.Respon
 		return nil, err
 	}
 	return resp, nil
+}
+
+// Utility function to make Get Requests a lot easier. 
+
+func RequestGet(requestUrl string, requestHeaders map[string]string) (*http.Response, error) {
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	req,_ := http.NewRequest("GET", requestUrl, nil)
+
+	for key, value := range (requestHeaders) {
+		req.Header.Add(key, value)
+	}
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println("Error", err)
+		return nil, err
+	}
+
+	return resp, nil
+	
 }
---
+++ utils/spotify.go
@@ -0,0 +1,82 @@
+package utils
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"os"
+)
+
+var accessToken string
+
+type Artist struct {
+	Name string `json:"name"`
+}
+
+type Track struct {
+	Name    string   `json:"name"`
+	Artists []Artist `json:"artists"`
+}
+
+type RecentlyPlayedItem struct {
+	Track    Track  `json:"track"`
+	PlayedAt string `json:"played_at"`
+}
+
+type RecentlyPlayedResponse struct {
+	Items []RecentlyPlayedItem `json:"items"`
+}
+
+
+var spotifyLogger *log.Logger
+
+func init() {
+	spotifyLogger = log.New(os.Stdout, "[SPOTIFY] ", log.LstdFlags)
+}
+
+//Gets the user's most recently listened to song from Spotify, using the access token and refreshing it if necessary. 
+func GetLastListened () (Track, error){
+	spotifyLogger.Println("Calling GetLastListened func")
+
+	// Check if the access token is currently initialized. If not, refresh it first
+	if accessToken == "" {
+		refreshedAccessToken, err := refreshAccessToken()
+
+		if err != nil {
+			spotifyLogger.Println("Error refreshing Access Token")
+			return Track{}, err
+		}
+
+		accessToken = refreshedAccessToken
+	}
+
+	// Use the Access token to call the Spotify endpoint for retrieving the user's most recently listened song.
+	headers := map[string]string {
+	"Authorization": "Bearer " + accessToken,
+	}
+
+	resp, err := RequestGet("https://api.spotify.com/v1/me/player/recently-played?limit=1", headers)
+
+	if err != nil {
+		spotifyLogger.Println("Error requesting last played song: %v", err)
+		return Track{}, err
+	}
+	defer resp.Body.Close()
+
+	responseBody, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		spotifyLogger.Println("Error reading response body: %v", err)
+		return Track{}, err
+	}
+
+	var responseResult RecentlyPlayedResponse
+	json.Unmarshal(responseBody, &responseResult)
+
+	if len(responseResult.Items) == 0 {
+		spotifyLogger.Println("No recently played tracks found")
+		return Track{}, nil
+	}
+
+	return responseResult.Items[0].Track, nil
+}
